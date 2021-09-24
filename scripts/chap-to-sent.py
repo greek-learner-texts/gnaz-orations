105700ded1291f15b4ab5982c7e12941af68b38f
James Tauber <jtauber@jtauber.com>
2021-09-24 15:05:17 +0800

    chapter and sentence segmentation for rest of Orations

--- scripts/chap-to-sent.py
+++ scripts/chap-to-sent.py
@@ -4,20 +4,21 @@
 converts the textpart-per-chap to textpart-per-sentence.
 """
 
+for n in ["27", "28", "29", "30", "31"]:
 
-input_filename = f"text/orat27.chap.txt"
-output_filename = f"text/orat27.sent.txt"
+    input_filename = f"text/orat{n}.chap.txt"
+    output_filename = f"text/orat{n}.sent.txt"
 
-with open(input_filename) as f, open(output_filename, "w") as g:
-    for line in f:
-        line = line.strip()
-        ref, text = line.split(maxsplit=1)
-        sentence = []
-        sent_num = 1
-        for token in text.split():
-            sentence.append(token)
-            if "." in token or ";" in token or "·" in token or "!" in token:
-                print(f"{ref}.{sent_num:02d} {' '.join(sentence)}", file=g)
-                sent_num += 1
-                sentence = []
-    assert sentence == [], f"finished para {ref} mid-sentence"
+    with open(input_filename) as f, open(output_filename, "w") as g:
+        for line in f:
+            line = line.strip()
+            ref, text = line.split(maxsplit=1)
+            sentence = []
+            sent_num = 1
+            for token in text.split():
+                sentence.append(token)
+                if "." in token or ";" in token or "·" in token or "!" in token:
+                    print(f"{ref}.{sent_num:02d} {' '.join(sentence)}", file=g)
+                    sent_num += 1
+                    sentence = []
+        assert sentence == [], f"finished para {ref} mid-sentence"
--- scripts/text-to-chap.py
+++ scripts/text-to-chap.py
@@ -6,25 +6,26 @@ converts the extracted files from OGL into a textpart-per-chapter format.
 
 import unicodedata
 
+for n in ["27", "28", "29", "30", "31"]:
 
-input_filename = f"text/orat27.txt"
-output_filename = f"text/orat27.chap.txt"
+    input_filename = f"text/orat{n}.txt"
+    output_filename = f"text/orat{n}.chap.txt"
 
-with open(input_filename) as f, open(output_filename, "w") as g:
-    chapter = 0
-    text = []
-    for line in f:
-        line = unicodedata.normalize("NFC", line.strip())
-        if line == "":  # blank line
-            pass
-        elif line.startswith("# "):
-            pass
-        elif line.startswith("## "):
-            if text:
-                print(f"{chapter:02d}", " ".join(text), sep="\t", file=g)
-            chapter += 1
-            text = []
-        else:
-            text.append(line)
+    with open(input_filename) as f, open(output_filename, "w") as g:
+        chapter = 0
+        text = []
+        for line in f:
+            line = unicodedata.normalize("NFC", line.strip())
+            if line == "":  # blank line
+                pass
+            elif line.startswith("# "):
+                pass
+            elif line.startswith("## "):
+                if text:
+                    print(f"{chapter:02d}", " ".join(text), sep="\t", file=g)
+                chapter += 1
+                text = []
+            else:
+                text.append(line)
 
-    print(f"{chapter:02d}", " ".join(text), sep="\t", file=g)
+        print(f"{chapter:02d}", " ".join(text), sep="\t", file=g)
